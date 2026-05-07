49cab2b88542209be18205f71d1e32f7639434ac
FatemehAssar <Fatemeh.assar@gmail.com>
2026-05-07 20:44:49 +0330

    remove extra project

--- Switch.Api/Models/OutboxMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Switch.Api.Models
-{
-    public class OutboxMessage
-    {
-        public Guid Id { get; set; }
-        public string Payload { get; set; }
-        public bool Processed { get; set; }
-    }
-}
--- Switch.Api/README.md
+++
@@ -1,6 +0,0 @@
-# Switch.Api
-## Architecture
-![Architecture](docs/sadad_architecture_diagram.png)
-
-## Sequence Diagram
-![Sequence](docs/sequence.png)
